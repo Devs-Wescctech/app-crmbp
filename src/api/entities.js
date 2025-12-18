b0373609a1ef15c415cdeac31a0f541bca25e52b
Wescctech <suporte@wescctech.com.br>
2025-12-17 22:20:23 -0300

    Add files via upload

---
+++ src/api/entities.js
@@ -0,0 +1,87 @@
+import { base44 } from './base44Client';
+
+
+export const Contact = base44.entities.Contact;
+
+export const Account = base44.entities.Account;
+
+export const Contract = base44.entities.Contract;
+
+export const Team = base44.entities.Team;
+
+export const Queue = base44.entities.Queue;
+
+export const Agent = base44.entities.Agent;
+
+export const SLAPolicy = base44.entities.SLAPolicy;
+
+export const Ticket = base44.entities.Ticket;
+
+export const TicketMessage = base44.entities.TicketMessage;
+
+export const Macro = base44.entities.Macro;
+
+export const Template = base44.entities.Template;
+
+export const CSATSurvey = base44.entities.CSATSurvey;
+
+export const TicketType = base44.entities.TicketType;
+
+export const Dependent = base44.entities.Dependent;
+
+export const KBCategory = base44.entities.KBCategory;
+
+export const KBArticle = base44.entities.KBArticle;
+
+export const KBFeedback = base44.entities.KBFeedback;
+
+export const KBArticleVersion = base44.entities.KBArticleVersion;
+
+export const Lead = base44.entities.Lead;
+
+export const Visit = base44.entities.Visit;
+
+export const Activity = base44.entities.Activity;
+
+export const Territory = base44.entities.Territory;
+
+export const SalesGoal = base44.entities.SalesGoal;
+
+export const SalesAgent = base44.entities.SalesAgent;
+
+export const ProposalTemplate = base44.entities.ProposalTemplate;
+
+export const Sale = base44.entities.Sale;
+
+export const PortalSession = base44.entities.PortalSession;
+
+export const SystemSettings = base44.entities.SystemSettings;
+
+export const Notification = base44.entities.Notification;
+
+export const NotificationPreference = base44.entities.NotificationPreference;
+
+export const LeadAutomation = base44.entities.LeadAutomation;
+
+export const Referral = base44.entities.Referral;
+
+export const ReferralActivity = base44.entities.ReferralActivity;
+
+export const DistributionRule = base44.entities.DistributionRule;
+
+export const QuickService = base44.entities.QuickService;
+
+export const LeadPJ = base44.entities.LeadPJ;
+
+export const ActivityPJ = base44.entities.ActivityPJ;
+
+export const LeadPJAutomation = base44.entities.LeadPJAutomation;
+
+export const CallAudit = base44.entities.CallAudit;
+
+export const QualityChecklist = base44.entities.QualityChecklist;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ src/api/functions.js
@@ -0,0 +1,59 @@
+import { base44 } from './base44Client';
+
+
+export const generateProposal = base44.functions.generateProposal;
+
+export const sendProposalWhatsApp = base44.functions.sendProposalWhatsApp;
+
+export const sendProposalEmail = base44.functions.sendProposalEmail;
+
+export const generateSignatureLink = base44.functions.generateSignatureLink;
+
+export const portalAuth = base44.functions.portalAuth;
+
+export const validateToken = base44.functions.validateToken;
+
+export const sendCollectionWhatsApp = base44.functions.sendCollectionWhatsApp;
+
+export const autentiqueCreateDocument = base44.functions.autentiqueCreateDocument;
+
+export const autentiqueCheckStatus = base44.functions.autentiqueCheckStatus;
+
+export const testAutentique = base44.functions.testAutentique;
+
+export const autentiqueGenerateLink = base44.functions.autentiqueGenerateLink;
+
+export const respondProposal = base44.functions.respondProposal;
+
+export const getPublicProposal = base44.functions.getPublicProposal;
+
+export const checkNotifications = base44.functions.checkNotifications;
+
+export const createNotification = base44.functions.createNotification;
+
+export const checkSLAWarnings = base44.functions.checkSLAWarnings;
+
+export const checkLeadAutomations = base44.functions.checkLeadAutomations;
+
+export const getCustomerFromERP = base44.functions.getCustomerFromERP;
+
+export const validateWhatsApp = base44.functions.validateWhatsApp;
+
+export const assignTicketRoundRobin = base44.functions.assignTicketRoundRobin;
+
+export const sendNpsSurvey = base44.functions.sendNpsSurvey;
+
+export const getNpsSurvey = base44.functions.getNpsSurvey;
+
+export const submitNpsSurvey = base44.functions.submitNpsSurvey;
+
+export const validateWhatsAppToken = base44.functions.validateWhatsAppToken;
+
+export const generateWhatsAppToken = base44.functions.generateWhatsAppToken;
+
+export const buscaCNPJ = base44.functions.buscaCNPJ;
+
+export const ai_assistant = base44.functions.ai_assistant;
+
+export const processCallAudit = base44.functions.processCallAudit;
+
